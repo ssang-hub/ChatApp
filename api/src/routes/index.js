7e68a811f236f246a2a7b082c6cc8bdddef1a617
ssang-hub <nguyendinhsang15102001@gmail.com>
2024-01-17 17:12:15 +0700

    clean code

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,19 +3,7 @@ import passport from '../controllers/passport';
 import user from './user';
 import authRoute from './auth';
 
-import test from './test';
-
 const route = express.Router();
-import multer from 'multer';
-
-// const upload = multer({ dest: "src/public/images/message/" });
-
-// // google login
-// route.get("/login/google", passport.authenticate("google", { scope: ["email", "profile"] }, { session: false }), (req, res, next) => {
-//   console.log(req.user._id);
-// });
-// route.get("/oauth2/redirect/google", passport.authenticate("google", { failureRedirect: "/OauthFaild", session: false }), genToken);
 route.use(authRoute);
-route.use(test);
 route.use(passport.authenticate('jwt', { session: false }), user);
 export default route;
